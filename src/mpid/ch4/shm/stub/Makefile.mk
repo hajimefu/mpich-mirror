3b1f52edadc2ddac0fc9f8bc4f9e650c6c397a7f
Charles J Archer <charles.j.archer@intel.com>
2015-08-28 12:08:11 -0700

    CH4:  compile fix.  Added missing stub_pre file for request types

--- src/mpid/ch4/shm/stub/Makefile.mk
+++ src/mpid/ch4/shm/stub/Makefile.mk
@@ -1,5 +1,7 @@
 if BUILD_CH4_SHM_STUB
 
+AM_CPPFLAGS        += -I$(top_srcdir)/src/mpid/ch4/shm/stub
+
 mpi_core_sources += src/mpid/ch4/shm/stub/ch4_shm_globals.c    \
                     src/mpid/ch4/shm/stub/ch4_shm_func_table.c
 lib_lib@MPILIBNAME@_la_CXXFLAGS  = -fno-rtti -fno-exceptions
---
+++ src/mpid/ch4/shm/stub/shm_stub_pre.h
@@ -0,0 +1,23 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ *  Portions of this code were written by Intel Corporation.
+ *  Copyright (C) 2011-2012 Intel Corporation.  Intel provides this material
+ *  to Argonne National Laboratory subject to Software Grant and Corporate
+ *  Contributor License Agreement dated February 8, 2012.
+ */
+
+#ifndef SHM_STUB_PRE_H_INCLUDED
+#define SHM_STUB_PRE_H_INCLUDED
+
+typedef struct {
+    int dummy;
+} MPIDI_shm_stub_amrequest_t;
+
+typedef struct {
+    int dummy;
+} MPIDI_shm_stub_request_t;
+
+#endif
