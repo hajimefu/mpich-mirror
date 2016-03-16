ebaa549be0491405b04c04458213d602a71e733d
Michael Chuvelev <michael.chuvelev@intel.com>
2016-03-16 17:52:06 +0300

    mpi_errno wasn't initialized

--- src/mpid/ch4/src/ch4_recv.h
+++ src/mpid/ch4/src/ch4_recv.h
@@ -170,6 +170,7 @@ __CH4_INLINE__ int MPIDI_Mrecv(void *buf,
     if (message == NULL) {
         /* treat as though MPI_MESSAGE_NO_PROC was passed */
         MPIR_Status_set_procnull(status);
+        mpi_errno = MPI_SUCCESS;
         goto fn_exit;
     }
 
