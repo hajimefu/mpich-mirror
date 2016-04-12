b7cab57be1fbee0fdc948f454ce59f7b4761f734
Wesley Bland <wesley.bland@intel.com>
2016-04-12 15:53:56 -0500

    CH4: Fix Intel copyrights

--- src/mpid/ch4/netmod/ofi/Makefile.mk
+++ src/mpid/ch4/netmod/ofi/Makefile.mk
@@ -4,6 +4,11 @@
 ## (C) 2011 by Argonne National Laboratory.
 ##     See COPYRIGHT in top-level directory.
 ##
+##  Portions of this code were written by Intel Corporation.
+##  Copyright (C) 2011-2016 Intel Corporation.  Intel provides this material
+##  to Argonne National Laboratory subject to Software Grant and Corporate
+##  Contributor License Agreement dated February 8, 2012.
+##
 if BUILD_CH4_NETMOD_OFI
 
 noinst_HEADERS     +=
