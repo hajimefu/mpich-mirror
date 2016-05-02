df7c556952473c68da3895a50908e31ab2f490d2
Yanfei Guo <yguo@anl.gov>
2016-05-02 17:33:29 -0500

    Fix LPID macros

--- src/mpid/ch4/include/mpidpre.h
+++ src/mpid/ch4/include/mpidpre.h
@@ -459,8 +459,8 @@ extern MPIDII_av_table_t *MPIDII_av_table0;
 #define MPIDIU_LPID_CREATE(avtid, lpid)      ((avtid << MPIDIU_LPID_BITS) | lpid)
 #define MPIDIU_LPID_GET_AVTID(lpid)          ((((lpid) & MPIDIU_AVTID_MASK) >> MPIDIU_LPID_BITS))
 #define MPIDIU_LPID_GET_LPID(lpid)           (((lpid) & MPIDIU_LPID_MASK))
-#define MPIDIU_LPID_SET_NEW_AVT_MARK(lpid)   ((lpid) | MPIDIU_NEW_AVT_MARK)
-#define MPIDIU_LPID_CLEAR_NEW_AVT_MARK(lpid) ((lpid) & (~MPIDIU_NEW_AVT_MARK))
+#define MPIDIU_LPID_SET_NEW_AVT_MARK(lpid)   ((lpid) |= MPIDIU_NEW_AVT_MARK)
+#define MPIDIU_LPID_CLEAR_NEW_AVT_MARK(lpid) ((lpid) &= (~MPIDIU_NEW_AVT_MARK))
 #define MPIDIU_LPID_IS_NEW_AVT(lpid)         ((lpid) & MPIDIU_NEW_AVT_MARK)
 
 
