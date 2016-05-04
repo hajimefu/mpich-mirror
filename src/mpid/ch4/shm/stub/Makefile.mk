c33a1fb836d07f9b4c7cdc6528e39662ed8d4e35
Ken Raffenetti <raffenet@mcs.anl.gov>
2016-05-04 12:18:32 -0500

    CH4/SHM/stub: remove unneeded CXXFLAGS

--- src/mpid/ch4/shm/stub/Makefile.mk
+++ src/mpid/ch4/shm/stub/Makefile.mk
@@ -2,7 +2,6 @@ if BUILD_CH4_SHM_STUB
 
 mpi_core_sources += src/mpid/ch4/shm/stub/globals.c    \
                     src/mpid/ch4/shm/stub/func_table.c
-lib_lib@MPILIBNAME@_la_CXXFLAGS  = -fno-rtti -fno-exceptions
 # errnames_txt_files += src/mpid/ch4/shm/stub/errnames.txt
 
 endif
