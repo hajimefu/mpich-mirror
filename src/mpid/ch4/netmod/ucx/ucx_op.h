caf02ac7c6e96244ea169deb369b9497636d7ce2
Ken Raffenetti <raffenet@mcs.anl.gov>
2016-05-11 15:27:50 -0500

    CH4/UCX: remove asserts from op hooks

--- src/mpid/ch4/netmod/ucx/ucx_op.h
+++ src/mpid/ch4/netmod/ucx/ucx_op.h
@@ -13,13 +13,11 @@
 
 static inline void MPIDI_NM_op_destroy(MPIR_Op *op_p)
 {
-    MPIU_Assert(0);
     return;
 }
 
 static inline void MPIDI_NM_op_commit(MPIR_Op *op_p)
 {
-    MPIU_Assert(0);
     return;
 }
 
